27d057419f7f38e99be0812eea643c2fd50cd5dd
Joyantichy <120155482+Joyantichy@users.noreply.github.com>
2023-02-28 17:16:26 +0600

    json added

--- js/json.js
+++ js/json.js
@@ -1,2 +1,31 @@
 const user = {id:1, name:'Gorib amir',job:'actor'};
-console.log(user)
+// javascript objects notation(json)
+const stringified = JSON.stringify(user);
+
+console.log(user);
+console.log(stringified);
+
+// { id: 1, name: 'Gorib amir', job: 'actor' }
+// {"id":1,"name":"Gorib amir","job":"actor"}
+
+const shop = {
+    owner : 'Alia',
+    address: {
+        street : 'kochukhet voot er goli',
+        city : 'ranbir',
+        country : 'BD'
+
+    },
+    products: ['laptop', 'maic', 'monitor' , 'keyboard'],
+    revenew : 45000,
+    isOpen :true,
+    isNew : false
+};
+console.log(shop);
+
+
+
+const shopJSON = JSON.stringify(shop);
+console.log(shopJSON);
+const shopobj = JSON.parse(shopJSON);
+console.log(shopobj)
